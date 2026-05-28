d1e53e0b44c56eb7a8bc69d537fef6321878b7b6
Tom Kawchak <thkawcha@microsoft.com>
2026-05-28 23:50:56 +0000

    Download script upgrades

--- download-latest-release-bits.sh
+++ download-latest-release-bits.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -euo pipefail
+
 RELEASE_PACKAGE_PIPELINE_ID=166
 RELEASE_PACKAGE_ARTIFACT_NAME=packages_amd64_retail_native
 RELEASE_PACKAGE_BUILD_ID=last_succeeded
-RELEASE_PACKAGES_FOLDER=/home/$USER/latest-release-packages
+RELEASE_PACKAGES_FOLDER="$HOME/latest-release-packages"
+REPO_ROOT="$HOME/meru-core"
 
-if [ -d "${RELEASE_PACKAGES_FOLDER}" ]; then
-    echo "Removing ${RELEASE_PACKAGES_FOLDER} folder"
-    rm -rf "${RELEASE_PACKAGES_FOLDER}"
-    mkdir -p "${RELEASE_PACKAGES_FOLDER}"
-fi
+echo "Removing ${RELEASE_PACKAGES_FOLDER} folder"
+sudo rm -rf "${RELEASE_PACKAGES_FOLDER}"
+mkdir -p "${RELEASE_PACKAGES_FOLDER}"
+sudo rm -f /tmp/poirot_context /tmp/poirot_context.tmp
 
 # az login --use-device-code
 
-./meru-core/ext/test-infra/src/test/systemtest/testfx/download-pipeline-artifact.sh \
+"${REPO_ROOT}/ext/test-infra/src/test/systemtest/testfx/download-pipeline-artifact.sh" \
     --pipeline-id $RELEASE_PACKAGE_PIPELINE_ID \
     --artifact-name $RELEASE_PACKAGE_ARTIFACT_NAME \
     --id $RELEASE_PACKAGE_BUILD_ID \
-    --destination $RELEASE_PACKAGES_FOLDER
+    --destination "$RELEASE_PACKAGES_FOLDER"
